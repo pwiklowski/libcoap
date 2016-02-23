d8b35f974f5272d17ad01deebdb753288d8b2c10
Pawel Wiklowski <pawwik@gmail.com>
2016-02-23 17:37:41 +0100

    Update make file for esp8266

--- component.mk
+++ component.mk
@@ -1,6 +1,5 @@
-INC_DIRS += $(libcoap_ROOT) 
-
-libcoap_INC_DIR = 
+INC_DIRS += $(libcoap_ROOT)
+libcoap_INC_DIR = $(libcoap_ROOT)
 libcoap_SRC_DIR = $(libcoap_ROOT)
 
 $(eval $(call component_compile_rules,libcoap))
